ad83c08e92a9571ea5271685d51cbd28ea8028fb
Kristijan98 <kristijan_98@live.com>
2021-08-08 14:38:32 +0200

    questions

--- Proekt Vizuelno/Form1.Designer.cs
+++ Proekt Vizuelno/Form1.Designer.cs
@@ -30,35 +30,103 @@ namespace Proekt_Vizuelno
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Proekt_Vizuelno.Properties.Resources.ZELENA;
-            this.pictureBox1.Location = new System.Drawing.Point(40, 26);
+            this.pictureBox1.Location = new System.Drawing.Point(43, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(496, 233);
+            this.pictureBox1.Size = new System.Drawing.Size(495, 231);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(270, 257);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(343, 268);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Tag = "1";
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(43, 268);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(190, 34);
+            this.button2.TabIndex = 3;
+            this.button2.Tag = "2";
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(343, 308);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(195, 33);
+            this.button3.TabIndex = 4;
+            this.button3.Tag = "3";
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(43, 308);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(190, 33);
+            this.button4.TabIndex = 5;
+            this.button4.Tag = "4";
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.checkAnswerEvent);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(577, 342);
+            this.ClientSize = new System.Drawing.Size(577, 364);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = " ";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Proekt Vizuelno/Form1.cs
+++ Proekt Vizuelno/Form1.cs
@@ -10,11 +10,356 @@ using System.Windows.Forms;
 
 namespace Proekt_Vizuelno
 {
-    public partial class Form1 : Form
-    {
+    public partial class Form1 : Form { 
+
+
+        int correctAnswer;
+        int questionNumber = 1;
+        int score;
+        int percentage;
+        int totalQuestions;
+
+
         public Form1()
-        {
-            InitializeComponent();
-        }
+    {
+        InitializeComponent();
+
+        askQuestion(questionNumber);
+
+        totalQuestions = 22;
     }
+
+    private void checkAnswerEvent(object sender, EventArgs e)
+    {
+        var senderObject = (Button)sender;
+
+        int buttonTag = Convert.ToInt32(senderObject.Tag);
+
+        if (buttonTag == correctAnswer)
+        {
+            score++;
+        }
+
+        if (questionNumber == totalQuestions)
+        {
+            // work out the percentage
+
+            percentage = (int)Math.Round((double)(score * 100) / totalQuestions);
+
+            MessageBox.Show(
+                "Квизот заврши!" + Environment.NewLine +
+                "Одговоривте " + score + " точни прашања." + Environment.NewLine +
+                "Процент на успешност " + percentage + "%" + Environment.NewLine +
+                "Кликнете ОК за повторно"
+                );
+
+            score = 0;
+            questionNumber = 0;
+            askQuestion(questionNumber);
+
+        }
+
+        questionNumber++;
+        askQuestion(questionNumber);
+
+    }
+
+    private void askQuestion(int qnum)
+    {
+
+        switch (qnum)
+        {
+            case 1:
+                pictureBox1.Image = Properties.Resources.GJEVREK;
+
+
+                button1.Text = "Розева";
+                button2.Text = "Бела";
+                button3.Text = "Ѓеврек";
+                button4.Text = "Црвена";
+
+                correctAnswer = 1;
+
+                break;
+            case 2:
+                pictureBox1.Image = Properties.Resources.GJEVREK1;
+
+
+                button1.Text = "Сина";
+                button2.Text = "Хоби";
+                button3.Text = "Ништо";
+                button4.Text = "Ѓеврек";
+
+                correctAnswer = 4;
+
+                break;
+            case 3:
+                pictureBox1.Image = Properties.Resources.PORTOKALOVA;
+
+
+                button1.Text = "Розева";
+                button2.Text = "Портокалова";
+                button3.Text = "Бела";
+                button4.Text = "Сина";
+
+                correctAnswer = 4;
+
+                break;
+            case 4:
+                pictureBox1.Image = Properties.Resources.REBRO;
+
+
+                button1.Text = "Црна";
+                button2.Text = "Ребро";
+                button3.Text = "Ништо";
+                button4.Text = "Бела";
+
+                correctAnswer = 3;
+
+                break;
+
+            case 5:
+                pictureBox1.Image = Properties.Resources.REBRO1;
+
+
+                button1.Text = "Ребро";
+                button2.Text = "Сина";
+                button3.Text = "Кафеава";
+                button4.Text = "Ништо";
+
+                correctAnswer = 1;
+
+                break;
+
+            case 6:
+                pictureBox1.Image = Properties.Resources.ROLETNA;
+
+
+                button1.Text = "Ролетна";
+                button2.Text = "Сина";
+                button3.Text = "Ништо";
+                button4.Text = "Кафеава";
+
+                correctAnswer = 3;
+
+                break;
+
+            case 7:
+                pictureBox1.Image = Properties.Resources.KAFENA;
+
+
+                button1.Text = "Сина";
+                button2.Text = "Ништо";
+                button3.Text = "Кафеава";
+                button4.Text = "Бела";
+
+                correctAnswer = 3;
+
+                break;
+
+            case 8:
+                pictureBox1.Image = Properties.Resources.HOBI;
+
+
+                button1.Text = "Бела";
+                button2.Text = "Ништо";
+                button3.Text = "Кафеава";
+                button4.Text = "Жолта";
+
+                correctAnswer = 4;
+
+                break;
+
+            case 9:
+                pictureBox1.Image = Properties.Resources.HOBI1;
+
+
+                button1.Text = "Ништо";
+                button2.Text = "Жолта";
+                button3.Text = "Хоби";
+                button4.Text = "Бела";
+
+                correctAnswer = 1;
+
+                break;
+
+
+            case 10:
+                pictureBox1.Image = Properties.Resources.HOBI2;
+
+
+                button1.Text = "Ништо";
+                button2.Text = "Жолта";
+                button3.Text = "Хоби";
+                button4.Text = "Бела";
+
+                correctAnswer = 3;
+
+                break;
+
+
+             case 11:
+                    pictureBox1.Image = Properties.Resources.LAPTOP;
+
+
+                    button1.Text = "Лаптоп";
+                    button2.Text = "Жолта";
+                    button3.Text = "Црвена";
+                    button4.Text = "Сина";
+
+                    correctAnswer = 1;
+
+                    break;
+
+
+             case 12:
+                    pictureBox1.Image = Properties.Resources.MAJA;
+
+
+                    button1.Text = "Ништо";
+                    button2.Text = "Маса";
+                    button3.Text = "Црвена";
+                    button4.Text = "Портокалева";
+
+                    correctAnswer = 2;
+
+                    break;
+
+             case 13:
+                    pictureBox1.Image = Properties.Resources.PORTOKALOVA1;
+
+
+                    button1.Text = "Ништо";
+                    button2.Text = "Портокалева";
+                    button3.Text = "Црна";
+                    button4.Text = "Бела";
+
+                    correctAnswer = 1;
+
+                    break;
+
+
+             case 14:
+                    pictureBox1.Image = Properties.Resources.PORTOKALOVA2;
+
+
+                    button1.Text = "Сина";
+                    button2.Text = "Портокалова";
+                    button3.Text = "Црвена";
+                    button4.Text = "Бела";
+
+                    correctAnswer = 3;
+
+                    break;
+
+
+             case 15:
+                    pictureBox1.Image = Properties.Resources.ROLETNA1;
+
+
+                    button1.Text = "Ролетна";
+                    button2.Text = "Сина";
+                    button3.Text = "Црвена";
+                    button4.Text = "Бела";
+
+                    correctAnswer = 1;
+
+                    break;
+
+
+             case 16:
+                    pictureBox1.Image = Properties.Resources.ROZEVA;
+
+
+                    button1.Text = "Црвена";
+                    button2.Text = "Розева";
+                    button3.Text = "Бела";
+                    button4.Text = "Ништо";
+
+                    correctAnswer = 1;
+
+                    break;
+             case 17:
+                    pictureBox1.Image = Properties.Resources.ROZEVA1;
+
+
+                    button1.Text = "Црвена";
+                    button2.Text = "Розева";
+                    button3.Text = "Жолта";
+                    button4.Text = "Розева";
+
+                    correctAnswer = 3;
+
+                    break;
+             case 18:
+                    pictureBox1.Image = Properties.Resources.TELEFON;
+
+
+                    button1.Text = "Црвена";
+                    button2.Text = "Жолта";
+                    button3.Text = "Зелена";
+                    button4.Text = "Телефон";
+
+                    correctAnswer = 4;
+
+                    break;
+
+              case 19:
+                    pictureBox1.Image = Properties.Resources.ZELENA;
+
+
+                    button1.Text = "Зелена";
+                    button2.Text = "Жолта";
+                    button3.Text = "Бела";
+                    button4.Text = "Портокалева";
+
+                    correctAnswer = 4;
+
+                    break;
+              case 20:
+                    pictureBox1.Image = Properties.Resources.ZELENA1;
+
+
+                    button1.Text = "Црвена";
+                    button2.Text = "Зелена";
+                    button3.Text = "Портокалева";
+                    button4.Text = "Розева";
+
+                    correctAnswer = 3;
+
+                    break;
+
+                case 21:
+                    pictureBox1.Image = Properties.Resources.ZOLTA;
+
+
+                    button1.Text = "Жолта";
+                    button2.Text = "Зелена";
+                    button3.Text = "Бела";
+                    button4.Text = "Розева";
+
+                    correctAnswer = 2;
+
+                    break;
+
+                case 22:
+                    pictureBox1.Image = Properties.Resources.ZOLTA1;
+
+
+                    button1.Text = "Жолта";
+                    button2.Text = "Зелена";
+                    button3.Text = "Бела";
+                    button4.Text = "Сина";
+
+                    correctAnswer = 4;
+
+                    break;
+
+            }
+
+
+
+        }
+
+}
 }
